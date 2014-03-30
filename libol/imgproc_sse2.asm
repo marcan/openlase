871f85159a88ed5f6c528c81462d8bfd0ab8fc21
Hector Martin <hector@marcansoft.com>
2014-03-31 00:54:22 +0100

    Add non-executable stack markings to imgproc_sse2.asm

--- libol/imgproc_sse2.asm
+++ libol/imgproc_sse2.asm
@@ -32,6 +32,16 @@
 
 %include "x86inc.asm"
 
+%ifidn __OUTPUT_FORMAT__,elf
+section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
+%ifidn __OUTPUT_FORMAT__,elf32
+section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
+%ifidn __OUTPUT_FORMAT__,elf64
+section .note.GNU-stack noalloc noexec nowrite progbits
+%endif
+
 SECTION .text
 INIT_XMM
 

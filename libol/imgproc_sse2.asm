7cfdadd809ed90ff8d559b96458b9ed58decfef1
Kamal Mostafa <kamal@whence.com>
2014-04-11 10:05:34 -0700

    Revert "Add non-executable stack markings to imgproc_sse2.asm"
    This reverts commit 871f85159a88ed5f6c528c81462d8bfd0ab8fc21.
    
    These declarations already exist in libol/x86inc.asm, so 871f851 has no useful
    effect but yields this new warning:
    
        libol/imgproc_sse2.asm:42: warning: section flags ignored on section redeclaration
    
    Signed-off-by: Kamal Mostafa <kamal@whence.com>

--- libol/imgproc_sse2.asm
+++ libol/imgproc_sse2.asm
@@ -32,16 +32,6 @@
 
 %include "x86inc.asm"
 
-%ifidn __OUTPUT_FORMAT__,elf
-section .note.GNU-stack noalloc noexec nowrite progbits
-%endif
-%ifidn __OUTPUT_FORMAT__,elf32
-section .note.GNU-stack noalloc noexec nowrite progbits
-%endif
-%ifidn __OUTPUT_FORMAT__,elf64
-section .note.GNU-stack noalloc noexec nowrite progbits
-%endif
-
 SECTION .text
 INIT_XMM
 
